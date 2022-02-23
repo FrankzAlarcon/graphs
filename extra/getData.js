613774e7fbf155e90c479e133bdeec3f278e1968
Frankz <leninalarcon7@gmail.com>
2022-02-22 22:41:28 -0500

    creados estilos minimos para completar la funcionalidad

--- extra/getData.js
+++ extra/getData.js
@@ -38,3 +38,31 @@ async function getDataAPI() {
 }
 
 export default getDataAPI;*/
+// import Twitter from 'twitter-v2';
+
+// let twitter  = new Twitter({
+//   consumer_key: 'eukrhMeOtHiGlpxzkzMaOUz9b',
+//   consumer_secret: 'W18F3RDZAb4VLGa7JXIhIFNQfz3HN4LR9IgD80jLniYxj6BYqO',
+//   access_token_key: '4865683485-OxjXay7efuS8rtImSZLSv4ocZqRUnCN4QEYq8bl',
+//   access_token_secret: '4RCc6jzLE9KksMJRS7eHwtdZY6lfwq0doGYlC2VMlqnXk'
+// });
+
+// /**Parametros: 1,2,3,4 */
+// async function getData(codigo) {
+//   const data = await twitter.get(`https://api.twitter.com/2/users/${codigo}/following?user.fields=public_metrics&max_results=10`)
+//   .then(response => {
+//     return response.data
+//   })
+//   .catch(error => console.log(error));
+
+//   return data || 'error';
+// }
+// async function getOwnerData(username) {
+//   const {data} = await twitter.get(`https://api.twitter.com/2/users/by/username/${username}?user.fields=public_metrics`).then(res => res.json())
+//   return data || 'error';
+// }
+
+// function get() {
+//   return getOwnerData('Frankz472');
+// }
+// console.log(get())
